df9cfcf0b67c3d76d68cf5118015f620090b0a06
Albert Klinkovský <albert.klinkovsky@gmail.com>
2022-12-29 20:07:35 +0100

    Init commit

---
+++ after/plugin/map.lua
@@ -0,0 +1,4 @@
+local map = require('reusek.map')
+local insert = map.insert
+
+-- insert('jk', '<esc>l')
---
+++ after/plugin/theme.lua
@@ -0,0 +1,16 @@
+-- vim.g.catppuccin_flavour = "macchiato"
+
+-- require("catppuccin").setup({
+-- 	flavout = "macchiato",
+-- 	integrations = {
+-- 		cmp = true,
+-- 		gitsigns = true,
+-- 		nvimtree = true,
+-- 		telescope = true,
+-- 		treesitter = true,
+-- 	}
+-- })
+
+-- vim.cmd [[colorscheme catppuccin]]
+
+vim.cmd [[colorscheme rose-pine]]
